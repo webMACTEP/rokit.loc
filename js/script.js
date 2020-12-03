8f3e7a93b6758d6d698e3dfb45061e500da01127
webMACTEP <70036631+webMACTEP@users.noreply.github.com>
2020-12-03 16:00:31 +0300

    objects__slider

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
    $('.header__slider').slick({
       dots: true
    });
+   $('.objects__slider').slick({
+      dots: true
+   });
    $('.selection__slider').slick({
       //dots: true
    });
